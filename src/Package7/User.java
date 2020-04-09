ac41c10c646e26325352270a44ad9fc56fc914f2
Cronus023 <anlrey.klochko@mail.ru>
2020-04-09 18:11:22 +0300

    add class User for users in our app

---
+++ src/Package7/User.java
@@ -0,0 +1,23 @@
+package Package7;
+
+public class User {
+	private String name;
+	private String addres;
+	
+	public User(String name, String addres) {
+		this.name = name;
+		this.addres = addres;
+	}
+	
+	public void displayUser(){
+		System.out.println(name + " " + addres);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getAddres() {
+		return addres;
+	}
+}
